0f8ef94599666bd9a57c22c6a2fc1110fb67a78b
Rahul Sharma <rahul.sharma@tatvasoft.com>
2026-01-22 14:05:16 +0530

    feature: UITextField Using UIViewRepresentable In SwiftUI. fix: nil refactor: nil

---
+++ SwiftUI_UIViewRepresentableDemo/SwiftUI_UIViewRepresentableDemo/SwiftUI_UIViewRepresentableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIViewRepresentableDemoApp.swift
+//  SwiftUI_UIViewRepresentableDemo
+//
+//  Created by MACM62 on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIViewRepresentableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
